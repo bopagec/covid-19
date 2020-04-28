735ba7585b31031b1909a084a3264c06bd1e9d5c
bopagep <pohodhika.bopage@uk.fujitsu.com>
2020-04-28 10:42:30 +0100

    enable swagger-2

--- src/main/java/com/blackpawsys/api/covid19/Covid19ApiApplication.java
+++ src/main/java/com/blackpawsys/api/covid19/Covid19ApiApplication.java
@@ -1,13 +1,22 @@
 package com.blackpawsys.api.covid19;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class Covid19ApiApplication {
 
   @Bean
@@ -17,6 +26,29 @@ public class Covid19ApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(Covid19ApiApplication.class, args);
+  }
+
+  @Bean
+  public Docket swaggerConfiguration(){
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .paths(PathSelectors.ant("/covid-19/report/**"))
+        .apis(RequestHandlerSelectors.basePackage("com.blackpawsys"))
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo(){
+    return new ApiInfo(
+        "COVID-19  T  R  A  C  K  E  R - API",
+        "COVID-19 Tracker 2020",
+        "1.0",
+        "Free to use",
+        new Contact("Pohodhika Bopage", "http://www.blackpawsys.co.uk", "blackpawsys@gmail.com"),
+        "API Licence",
+        "http://www.blackpawsys.co.uk",
+        Collections.emptyList()
+        );
   }
 
 }
